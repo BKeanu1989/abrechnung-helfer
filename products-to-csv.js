d32be48cf063098d84a5b0170426513cd5b8731b
Kevin Fechner <developer.kevinfechner@gmail.com>
2019-03-27 13:18:36 +0100

    add start & end to filename; create error file

--- products-to-csv.js
+++ products-to-csv.js
@@ -1,9 +1,10 @@
 #!/usr/bin/env node
 const fs = require('fs')
 const path = require('path')
-
 const program = require('commander')
 const Json2csvParser = require('json2csv').Parser;
+
+const {baseUrl, urlEnding} = require('./config.json')
 
 program
     .arguments('<file> [options]')
@@ -14,6 +15,8 @@ program
     })
     .parse(process.argv)
 
+let errors = []
+
 const inputFile = path.resolve(process.cwd(), input)
 const artistData_string = fs.readFileSync(inputFile, 'utf8')
 
@@ -21,8 +24,20 @@ const {name} = path.parse(inputFile)
 
 const artistData = JSON.parse(artistData_string)
 
+const {start, end} = artistData.timeFrame
 const products = artistData.products
 
 const json2csvParser = new Json2csvParser()
-const csv = json2csvParser.parse(products)
-const writer = fs.writeFileSync(`./${name}-products.csv`, csv)
+const valid_csv = json2csvParser.parse(products)
+const writer = fs.writeFileSync(`./${name}-products-${start}-${end}.csv`, valid_csv)
+
+products.forEach((product) => {
+    let valid = (product['_qty']) ? true : false
+    product.order_url = `${baseUrl}${product['order_id']}${urlEnding}`
+    if (!valid) {
+        errors.push(product)
+    }
+})
+
+const error_csv = json2csvParser.parse(errors)
+fs.writeFileSync(`./${name}-products-errors-${start}-${end}.csv`, error_csv)
